2fdec4017b83773d14a7228e83d1d91ef49d51f6
iencatalin <ienulesccatalin@gmail.com>
2026-02-08 16:10:04 +0200

    Add better auth

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+export default function page() {
+  return (
+    <div className='px-8 py-16 mx-auto max-w-5xl space-y-8'>
+      <div className='space-y-8'>
+        <h1 className='text-3xl font-bold'>Register</h1>
+      </div>
+    </div>
+  );
+}
