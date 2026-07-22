19f213617104e8643c958a3b0c0124365a6e725f
iencatalin <ienulesccatalin@gmail.com>
2026-07-22 18:11:29 +0300

    switch to prisma-client-js default path

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
+  /* config options here */
+
   devIndicators: false,
 };
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 
-import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { nextCookies } from 'better-auth/next-js';
 
 const prisma = new PrismaClient();
