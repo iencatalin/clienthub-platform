592274db980d0917bf6dce92a671edb3b2c40c47
iencatalin <ienulesccatalin@gmail.com>
2026-07-22 18:03:49 +0300

    fix prisma engine path

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    '/*': ['./src/generated/prisma/**/*'],
-  },
-
   devIndicators: false,
 };
 
