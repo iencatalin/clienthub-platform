4d01505b41c40c14fbb7fdd4849f86bf47821f76
iencatalin <ienulesccatalin@gmail.com>
2026-07-22 17:49:16 +0300

    fix prisma binary

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    '/*': ['./src/generated/prisma/**/*'],
+  },
 
   devIndicators: false,
 };
